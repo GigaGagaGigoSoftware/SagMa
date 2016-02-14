82831e52c5718dbdcbec004d3a5b6f62b3207d61
Steven <darkmamba1337@gmail.com>
2016-02-14 14:06:38 +0100

    FXML-GUI hinzugefügt

---
+++ FXMLGui/src/de/gigagagagigo/sagma/client/ui/fxml/Main.java
@@ -0,0 +1,34 @@
+package de.gigagagagigo.sagma.client.ui.fxml;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			
+			FXMLLoader loader = new FXMLLoader();
+			loader.setResources(ResourceBundle.getBundle("language\\chat", new Locale("en", "EN")));
+			BorderPane root = loader.load(getClass().getResource("chat.fxml").openStream());
+			primaryStage.setTitle("SagMa");
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("blackstyle.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}

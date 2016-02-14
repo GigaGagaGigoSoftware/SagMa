01b2b3849b45c8afdc15b057486f7da256b1335a
Steven <darkmamba1337@gmail.com>
2016-02-14 15:17:32 +0100

    -Styling des Chats -Umstrukturierung der MenuItems

--- FXMLGui/src/de/gigagagagigo/sagma/client/ui/fxml/Main.java
+++ FXMLGui/src/de/gigagagagigo/sagma/client/ui/fxml/Main.java
@@ -19,7 +19,9 @@ public class Main extends Application {
 			loader.setResources(ResourceBundle.getBundle("language\\chat", new Locale("en", "EN")));
 			BorderPane root = loader.load(getClass().getResource("chat.fxml").openStream());
 			primaryStage.setTitle("SagMa");
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,550,550);
+			primaryStage.setMinHeight(550);
+			primaryStage.setMinWidth(350);
 			scene.getStylesheets().add(getClass().getResource("blackstyle.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();

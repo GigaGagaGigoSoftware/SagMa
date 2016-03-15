654514ac9e5aa95904e4f7655bd0110d20bd6e22
Green Lightning <GreenLightning.git@googlemail.com>
2016-03-15 13:14:47 +0100

    Update client ui test.

--- ClientUITest/src/de/gigagagagigo/sagma/client/ui/test/Main.java
+++ ClientUITest/src/de/gigagagagigo/sagma/client/ui/test/Main.java
@@ -7,29 +7,38 @@ import de.gigagagagigo.sagma.net.NetworkFactories;
 
 public class Main {
 
-	private static final boolean SWING = true;
-	private static final int CLIENTS = 2;
+	private static final boolean SERVER_GUI = true;
+	private static final boolean SWING = false;
+	private static final int SWING_CLIENTS = 2;
 
 	public static void main(String[] args) {
 		NetworkFactories.set(new LocalNetworkFactory());
 		startServer();
-		for (int i = 0; i < CLIENTS; i++) {
-			if (SWING) {
-				de.gigagagagigo.sagma.client.ui.swing.Main.main(new String[0]);
-			} else {
-				de.gigagagagigo.sagma.client.ui.fxml.Main.main(new String[0]);
-			}
-		}
+		startClients();
 	}
 
 	private static void startServer() {
-		new Thread(() -> {
-			try {
-				de.gigagagagigo.server.cli.Main.main(new String[0]);
-			} catch (IOException e) {
-				e.printStackTrace();
+		if (SERVER_GUI) {
+			de.gigagagagigo.sagma.server.ui.swing.Main.main(new String[0]);
+		} else {
+			new Thread(() -> {
+				try {
+					de.gigagagagigo.server.cli.Main.main(new String[0]);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}).start();
+		}
+	}
+
+	private static void startClients() {
+		if (SWING) {
+			for (int i = 0; i < SWING_CLIENTS; i++) {
+				de.gigagagagigo.sagma.client.ui.swing.Main.main(new String[0]);
 			}
-		}).start();
+		} else {
+			de.gigagagagigo.sagma.client.ui.fxml.Main.main(new String[0]);
+		}
 	}
 
 }
